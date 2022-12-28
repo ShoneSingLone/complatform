76032b92be624285f69787269f259a2abcc7fbc4
ShoneSingLone <ShoneSingLone@gmail.com>
2022-12-28 09:51:20 +0800

    dev jsonschemamonacoeditor

--- src/components/ViewNotFound.tsx
+++ src/components/ViewNotFound.tsx
@@ -1,6 +1,5 @@
 import { defineComponent } from "vue";
 import { Cpt_url } from "../router/router";
-import { JsonSchemaMonaco } from "./JsonSchemaEditor/JsonSchemaMonaco";
 
 export const ViewNotFound = defineComponent({
 	props: ["pathname"],
@@ -21,69 +20,9 @@ export const ViewNotFound = defineComponent({
 	},
 	data(vm) {
 		return {
-			value: `{
-				"$schema": "http://json-schema.org/draft-04/schema#",
-				"type": "object",
-				"properties": {
-					"auth": {
-						"type": "object",
-						"properties": {
-							"identity": {
-								"type": "object",
-								"properties": {
-									"methods": {
-										"type": "array",
-										"items": {
-											"type": "string"
-										}
-									},
-									"password": {
-										"type": "object",
-										"properties": {
-											"user": {
-												"type": "object",
-												"properties": {
-													"name": {
-														"type": "string"
-													},
-													"password": {
-														"type": "string"
-													},
-													"domain": {
-														"type": "object",
-														"properties": {
-															"name": {
-																"type": "string"
-															}
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							},
-							"scope": {
-								"type": "object",
-								"properties": {
-									"domain": {
-										"type": "object",
-										"properties": {
-											"name": {
-												"type": "string"
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}`
 		}
 	},
 	render() {
-		return <div style="height:500px;overflow:auto;"><JsonSchemaMonaco schemaString={this.value} /></div>
 		return (
 			<aResult status="404" title="404" subTitle={this.pathname} class="flex1">
 				{{
